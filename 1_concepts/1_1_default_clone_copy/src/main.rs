c80e6c46c9374358c692c3905351f58f4e243db5
Andrii Sokyrko <wartwvister@gmail.com>
2023-07-23 15:59:09 +0200

    test

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Implement me!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_copy() {
+        let a = 1;
+        let b = a;
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn test_clone() {
+        let a = String::from("Hello");
+        let b = a.clone();
+        assert_eq!(a, b);
+    }
+}
